635b147f1f45bd94534bd4e22d1e66090d86538e
Dhananjay Lokhande <125077594+Deadpool1975@users.noreply.github.com>
2023-07-07 16:22:54 +0530

    Add files via upload
    added apis, app functions and urls for  differenr web pages of joke search.

---
+++ giggle/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Joke(models.Model):
+    query = models.CharField(max_length=50)
+    response = models.TextField()
+    created_at = models.DateTimeField(
+        verbose_name='Created at',
+        auto_now_add=True,
+    )
+    
+    def __str__(self):
+        return f"{self.query} - {self.created_at}"
+        
